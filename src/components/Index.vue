89e8f4f735c3875d2ef4b3d1b8051237ddcb499e
LeanLeeOne <yahaheihei@foxmail.com>
2021-10-13 16:25:14 +0800

    改进悬浮颜色

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,16 +3,17 @@
     <img id="logo" alt="Vue logo" src="../assets/moon.png">
     <h1 hidden>{{ msg }}</h1>
     <input placeholder="搜索框" hidden>
-    <ul title="高斯模糊、响应式布局">
+    <ul>
       <li>
-        <a href="http://blog.leanlee.top"
-           rel="noopener">
+        <a href="/blog/"
+           class="blog">
           <i class="iconfont icon-ningmeng"></i>
           <span>Blog</span>
         </a>
       </li>
       <li>
         <a href="https://www.cnblogs.com/lean-lee/"
+           class="cnblogs"
            target="_blank"
            rel="noopener">
           <i class="iconfont icon-cnblogs"></i>
@@ -21,6 +22,7 @@
       </li>
       <li>
         <a href="https://github.com/LeanLeeOne/"
+           class="github"
            target="_blank"
            rel="noopener">
           <i class="iconfont icon-github"></i>
@@ -29,6 +31,7 @@
       </li>
       <li>
         <a href="https://space.bilibili.com/13913026"
+           class="bilibili"
            target="_blank"
            rel="noopener">
           <i class="iconfont icon-bilibili"></i>
@@ -37,7 +40,7 @@
       </li>
       <li>
         <a href="/wechat/"
-           rel="noopener">
+           class="wechat">
           <i class="iconfont icon-weixin"></i>
           <span>Wechat</span>
         </a>
@@ -49,10 +52,10 @@
 
 <script>
 export default {
-    name: 'Index',
-    props: {
-        msg: String
-    }
+  name: 'Index',
+  props: {
+    msg: String
+  }
 }
 </script>
 
@@ -82,8 +85,7 @@ li i {
 li i:hover {
   transform: scale(1.2);
 }
-a,
-router-link {
+a {
   color: LemonChiffon;
   text-decoration: none;
   height: 100%;
@@ -99,9 +101,20 @@ router-link {
   transition: color .25s ease-out .1s;
   text-shadow: 2px 2px 4px #00000070;
 }
-a:hover,
-router-link:hover {
-  color: limegreen;
+a.blog:hover {
+  color: #f9f967;
+}
+a.cnblogs:hover {
+  color: #1296db;
+}
+a.github:hover {
+  color: #3e75c3;
+}
+a.bilibili:hover {
+  color: #1296db;
+}
+a.wechat:hover {
+  color: #09bb07;
 }
 .beian {
   width: 100%;
@@ -109,5 +122,8 @@ router-link:hover {
   position: fixed;
   bottom: 20px;
   left: 0;
+}
+.beian:hover {
+  color: limegreen;
 }
 </style>
