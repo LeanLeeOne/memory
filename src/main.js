042949574a91047de154586722138a98b3c9ca47
LeanLeeOne <yahaheihei@foxmail.com>
2021-10-13 16:13:12 +0800

    完善标题显示

--- src/main.js
+++ src/main.js
@@ -12,13 +12,39 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: "history",
   routes: [
-    {path: "/", component: Index},
-    {path: "/blog/", component: Blog},
-    {path: "/wechat/", component: Wechat}
+    {
+      path: "/",
+      component: Index,
+      meta: {
+        title: "李晓辉的个人网站"
+      }
+    },
+    {
+      path: "/blog/",
+      component: Blog,
+      meta: {
+        title: "博客"
+      }
+    },
+    {
+      path: "/wechat/",
+      component: Wechat,
+      meta: {
+        title: "微信"
+      }
+    }
   ]
 });
 
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = "檬胧 | " + to.meta.title;
+  }
+  next()
+});
+
 new Vue({
-    render: h => h(App),
-    router
+  render: h => h(App),
+  router
 }).$mount('#app');
