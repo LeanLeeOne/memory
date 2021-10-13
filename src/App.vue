7ab0ea938868c79fcfb9c82f2e86019aad000580
LeanLeeOne <yahaheihei@foxmail.com>
2021-10-13 13:19:25 +0800

    调整页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img style="height: 800px" alt="Vue logo" src="./assets/logo.png">
+    <Index msg="檬胧"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="main">
+    <h1 hidden>{{ msg }}</h1>
+    <input placeholder="搜索框" hidden>
+    <ul title="高斯模糊、响应式布局">
+      <li>
+        <a href="http://blog.leanlee.top"
+           rel="noopener">
+          <i class="iconfont ic-blog"></i>
+          <span>Blog</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.cnblogs.com/lean-lee/"
+           target="_blank"
+           rel="noopener">
+          <i class="iconfont ic-cnblogs"></i>
+          <span>CNBlogs</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/LeanLeeOne/"
+           target="_blank"
+           rel="noopener">
+          <i class="iconfont ic-github"></i>
+          <span>Github</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://space.bilibili.com/13913026"
+           target="_blank"
+           rel="noopener">
+          <i class="iconfont ic-bilibili"></i>
+          <span>Bilibili</span>
+        </a>
+      </li>
+      <li>
+        <a href="wechat.leanlee.top"
+           target="_blank"
+           rel="noopener">
+          <i class="iconfont ic-wechat"></i>
+          <span>Wechat</span>
+        </a>
+      </li>
+    </ul>
+    <a class="beian" href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备18052178号-1</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+@import "../assets/font.css";
+.main {
+
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  width: 60px;
+  height: 60px;
+  margin: 0 10px;
+  padding: 10px;
+  display: inline-block;
+}
+li i{
+  font-size: 35px;
+  transition: all 0.3s;
+}
+li i:hover{
+  transform: scale(1.2);
+}
+a {
+  color: LemonChiffon;
+  text-decoration:none;
+  height: 100%;
+  font-size: 13px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  -webkit-transition: color .25s ease-out .1s;
+  -moz-transition: color .25s ease-out .1s;
+  -o-transition: color .25s ease-out .1s;
+  -ms-transition: color .25s ease-out .1s;
+  transition: color .25s ease-out .1s;
+  text-shadow: 2px 2px 4px #00000070;
+}
+a:hover{
+  color: limegreen;
+}
+.beian{
+  width: 100%;
+  height: 1em;
+  position: fixed;
+  bottom: 20px;
+  left: 0;
+}
+</style>
