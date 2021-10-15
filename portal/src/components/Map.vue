95fda47ba43c67f3b4a1ef668b621254e7ae58c5
LeanLeeOne <yahaheihei@foxmail.com>
2021-10-15 13:48:59 +0800

    增加折叠、定位

--- portal/src/components/Map.vue
+++ portal/src/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <baidu-map class="map" center="北京">
+    <baidu-map class="map" :center="center" :zoom="zoom" @ready="resetMap">
       <bm-marker v-for="(r, i) in restaurants" :key="'p-'+r.id"
                  :position="{lng:r.longitude,lat:r.latitude}">
         <bm-label :content="(i+1)+'.'+r.name"
@@ -9,6 +9,19 @@
       </bm-marker>
     </baidu-map>
     <div class="list">
+      <div class="menu">
+        <Button type="success"
+                @click="collapse"
+                v-show="!isCollapse">收起
+        </Button>
+        <Button type="error"
+                @click="collapse"
+                v-show="isCollapse">展开
+        </Button>
+        <Button type="info"
+                @click="resetMap">重置地图
+        </Button>
+      </div>
       <Restaurant v-for="(r, i) in restaurants" :key="'r-'+r.id"
                   :index="i"
                   :name="r.name"
@@ -17,7 +30,9 @@
                   :timestamp="r.timestamp"
                   :address="r.address"
                   :summary="r.summary"
-                  :url="r.url">
+                  :url="r.url"
+                  v-show="!isCollapse"
+                  @click.native="setCenter(r.longitude, r.latitude)">
       </Restaurant>
     </div>
   </div>
@@ -30,7 +45,10 @@ export default {
   name: "Map",
   data() {
     return {
+      center: {lng: 0, lat: 0},
+      zoom: 3,
       restaurants: [],
+      isCollapse: false,
       sites: [],
       label: {
         style: {
@@ -48,19 +66,40 @@ export default {
     Restaurant
   },
   mounted() {
-    // 最近
-    this.$axios
-      .get("/api/restaurant/query/")
-      .then(response => {
-        this.restaurants = response.data;
-      });
-
-    // 所有点位
-    this.$axios
-      .get("/api/restaurant/sites/")
-      .then(response => {
-        this.sites = response.data;
-      });
+    this.drawRecent();
+    this.drawAllSite();
+  },
+  methods: {
+    resetMap({BMap, map}) {
+      BMap && map && console.log(BMap, map);
+      this.setCenter(116.404001, 39.915001, 12);
+      setTimeout(() => {
+        this.setCenter(116.404, 39.915, 12);
+      }, 100);
+    },
+    setCenter(lng, lat, zoom) {
+      zoom || (zoom = 14);
+      this.center.lng = lng;
+      this.center.lat = lat;
+      this.zoom = zoom;
+    },
+    drawRecent() { // 最近
+      this.$axios
+        .get("/api/restaurant/query/")
+        .then(response => {
+          this.restaurants = response.data;
+        });
+    },
+    drawAllSite() { // 所有点位
+      this.$axios
+        .get("/api/restaurant/sites/")
+        .then(response => {
+          this.sites = response.data;
+        });
+    },
+    collapse() {
+      this.isCollapse = !this.isCollapse;
+    }
   }
 }
 </script>
@@ -74,5 +113,9 @@ export default {
   position: absolute;
   top: 1em;
   left: 1em;
+  text-align: left;
+}
+.menu button:nth-child(3) {
+  margin-left: 3px;
 }
 </style>
