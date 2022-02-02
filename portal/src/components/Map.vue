7f88c3deb4b3fbd5bae4e30856b11604d0ceb360
李晓辉 <yahaheihei@foxmail.com>
2022-02-02 11:33:00 +0800

    格式化代码

--- portal/src/components/Map.vue
+++ portal/src/components/Map.vue
@@ -12,11 +12,10 @@
                   :labelStyle="label.style"
                   :offset="label.offset"/>
       </bm-marker>
-      <bm-point-collection
-          :points="sites"
-          shape="BMAP_POINT_SHAPE_CIRCLE"
-          size="BMAP_POINT_SIZE_SMALL"
-          color="red">
+      <bm-point-collection :points="sites"
+                           shape="BMAP_POINT_SHAPE_CIRCLE"
+                           size="BMAP_POINT_SIZE_SMALL"
+                           color="red">
       </bm-point-collection>
     </baidu-map>
     <div class="left-panel">
@@ -64,34 +63,36 @@
               @click="showAddRestaurantView">
       </Button>
     </div>
-    <Modal
-        v-model="addRestaurantView"
-        title="饭店信息"
-        :loading="true"
-        ok-text="增加"
-        @on-ok="validateRestaurant('addRestaurantModel')"
-        @on-cancel="hideAddRestaurantView">
-      <Form
-          ref="addRestaurantModel"
-          :model="addRestaurantModel"
-          :rules="addRestaurantRule"
-          :label-width="100">
-        <Form-item label="视频地址" prop="url">
+    <Modal v-model="addRestaurantView"
+           title="饭店信息"
+           :loading="true"
+           ok-text="增加"
+           @on-ok="validateRestaurant('addRestaurantModel')"
+           @on-cancel="hideAddRestaurantView">
+      <Form ref="addRestaurantModel"
+            :model="addRestaurantModel"
+            :rules="addRestaurantRule"
+            :label-width="100">
+        <Form-item label="视频地址"
+                   prop="url">
           <Input v-model="addRestaurantModel.url"
                  placeholder="请输入视频地址">
           </Input>
         </Form-item>
-        <Form-item label="饭店名" prop="name">
+        <Form-item label="饭店名"
+                   prop="name">
           <Input v-model="addRestaurantModel.name"
                  placeholder="请输入饭店名">
           </Input>
         </Form-item>
-        <Form-item label="消费金额" prop="total">
+        <Form-item label="消费金额"
+                   prop="total">
           <Input v-model="addRestaurantModel.total"
                  placeholder="请输入消费金额">
           </Input>
         </Form-item>
-        <Form-item label="就餐人数" prop="guests">
+        <Form-item label="就餐人数"
+                   prop="guests">
           <Input v-model="addRestaurantModel.guests"
                  placeholder="请选择人数">
           </Input>
@@ -101,33 +102,39 @@
                   :max="10">
           </Slider>
         </Form-item>
-        <Form-item label="探店时间" prop="timestamp">
+        <Form-item label="探店时间"
+                   prop="timestamp">
           <Input v-model="addRestaurantModel.timestamp"
                  placeholder="请输入探店时间">
           </Input>
         </Form-item>
-        <Form-item label="地址" prop="address">
+        <Form-item label="地址"
+                   prop="address">
           <Input v-model="addRestaurantModel.address"
                  placeholder="请输入地址">
           </Input>
         </Form-item>
-        <Form-item label="经度" prop="longitude">
+        <Form-item label="经度"
+                   prop="longitude">
           <Input v-model="addRestaurantModel.longitude"
                  placeholder="请输入经度">
           </Input>
         </Form-item>
-        <Form-item label="纬度" prop="latitude">
+        <Form-item label="纬度"
+                   prop="latitude">
           <Input v-model="addRestaurantModel.latitude"
                  placeholder="请输入纬度">
           </Input>
         </Form-item>
-        <Form-item label="是否有分店" prop="branch">
+        <Form-item label="是否有分店"
+                   prop="branch">
           <Radio-group v-model="addRestaurantModel.branch">
             <Radio :label="0">否</Radio>
             <Radio :label="1">是</Radio>
           </Radio-group>
         </Form-item>
-        <Form-item label="总结" prop="summary">
+        <Form-item label="总结"
+                   prop="summary">
           <Input v-model="addRestaurantModel.summary"
                  placeholder="请输入总结">
           </Input>
